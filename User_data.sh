14755bb22a22fb867cca5ed63ef52c069f1be1dd
AKIS-4 <abhishekharkar@gmail.com>
2025-07-25 20:37:12 +0530

    updated

--- User_data.sh
+++ User_data.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y docker.io docker-compose
 sudo usermod -aG docker ubuntu
 sudo systemctl enable docker
 sudo systemctl start docker
-sleep 10
+sleep 20
 cd /home/ubuntu/docks
 docker-compose up -d
